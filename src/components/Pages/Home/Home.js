5d7d72eb65c51f4f338543bdbc0a280417f60334
Sohel-Developer <sohel.webdevoloper.com>
2022-10-26 11:45:08 +0600

    Hero Section Completed

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -4,7 +4,7 @@ import HeroSection from '../../HeroSection/HeroSection';
 const Home = () => {
     return (
         <div>
-            {/* <HeroSection /> */}
+            <HeroSection />
         </div>
     );
 };
